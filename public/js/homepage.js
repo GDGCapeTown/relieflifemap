55175bdcd0f6a2513773a9a40254354a2d1a225e
Rich Ard <flush3dwork@gmail.com>
2013-09-14 20:39:36 +0200

    removed drawingMode from starting in marker mode

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -20,26 +20,14 @@ function initialize() {
 	load_events_from_map();
 
 	var drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.MARKER,
     drawingControl: true,
     drawingControlOptions: {
       position: google.maps.ControlPosition.TOP_CENTER,
       drawingModes: [
         google.maps.drawing.OverlayType.POLYGON
       ]
-    },
-    markerOptions: {
-      icon: 'images/beachflag.png'
-    },
-    circleOptions: {
-      fillColor: '#ffff00',
-      fillOpacity: 1,
-      strokeWeight: 5,
-      clickable: false,
-      editable: true,
-      zIndex: 1
     }
-  });
+});
   drawingManager.setMap(map);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
