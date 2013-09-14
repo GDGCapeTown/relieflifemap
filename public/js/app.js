3ce5246cae47e3520402cb8bb7c437b0e95230a0
Johann du Toit <exxonno@gmail.com>
2013-09-14 22:03:13 +0200

    Added drawing

--- public/js/app.js
+++ public/js/app.js
@@ -200,6 +200,30 @@
 		google.maps.event.addListener(main_map, 'center_changed', handle_center_change_of_map);
 
 		// select-point-map-canvas
+		select_map = new google.maps.Map(document.getElementById('select-point-map-canvas'), mapOptions);
+
+		var drawingManager = new google.maps.drawing.DrawingManager({
+		drawingMode: google.maps.drawing.OverlayType.MARKER,
+		drawingControl: true,
+		drawingControlOptions: {
+		position: google.maps.ControlPosition.TOP_CENTER,
+		drawingModes: [
+		google.maps.drawing.OverlayType.POLYGON
+		]
+		},
+		markerOptions: {
+		icon: 'images/beachflag.png'
+		},
+		circleOptions: {
+		fillColor: '#ffff00',
+		fillOpacity: 1,
+		strokeWeight: 5,
+		clickable: false,
+		editable: true,
+		zIndex: 1
+		}
+		});
+		drawingManager.setMap(select_map);
 
 		// Setup center
 		handle_center_change_of_map();
